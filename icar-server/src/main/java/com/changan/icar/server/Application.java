cabd5472c96ca9acee136b846d49b9bd24c9cebc
ljh <578525306@qq.com>
2018-05-11 16:48:35 +0800

    dubbo 测试1.5

--- icar-server/src/main/java/com/changan/icar/server/Application.java
+++ icar-server/src/main/java/com/changan/icar/server/Application.java
@@ -3,10 +3,18 @@ package com.changan.icar.server;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.changan.icar.server.dao")
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
